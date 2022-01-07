271796ebf1f1ff714aa67cc228cc24ee8379cd3c
Nedbor <rainier507@gmail.com>
2022-01-07 01:57:41 -0500

    Se cambio la tematica de la pagina de año nuevo a navidad

--- code.js
+++ code.js
@@ -32,7 +32,7 @@ const countdown = (deadline, countdown, finalMessage) => {
     },1000)
 }
 
-countdown('Jan 01 2023 00:00:00 GMT-5', 'countdown', 'Happy New Year');
+countdown('Dec 25 2022 00:00:00 GMT-5', 'countdown', 'Happy Xmas');
 
 // Some Snow Falling by Codepen.io
 
