51437bb743542fe7d56a7000cadb04e400fed4df
Rainier Ríos <96790657+Nedbor@users.noreply.github.com>
2022-12-25 15:57:16 -0500

    commit final mensaje final listo en ios y en andoid

--- code.js
+++ code.js
@@ -40,7 +40,7 @@ $secondsNode = document.getElementById('seconds'),
 $finalMessage = document.querySelector('.final-sms');
 
 const currentYear = new Date().getFullYear();
-const newyear = new Date(`January 01 ${currentYear }`);
+const newyear = new Date(`January 01 ${currentYear + 1}`);
 
 const timeCountDown = () => {
     const currentDate = new Date();
@@ -64,7 +64,7 @@ const timeCountDown = () => {
 
 };
 
-const formatTime = (time) => time > 10 ? time : `0${time}`;
+const formatTime = (time) => time > 10 ? time : `${time}`;
 
 timeCountDown();
 setInterval(timeCountDown, 1000);
