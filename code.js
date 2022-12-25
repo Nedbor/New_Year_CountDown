9398e8241aaacde091803559aad4b1b8169ba572
Rainier Ríos <96790657+Nedbor@users.noreply.github.com>
2022-12-25 16:54:05 -0500

    pruebas

--- code.js
+++ code.js
@@ -1,39 +1,4 @@
-const $days = document.getElementById('days'),
-$hours = document.getElementById('hours'),
-$minutes = document.getElementById('minutes'),
-$seconds = document.getElementById('seconds'),
-$finalMessage = document.getElementsByClassName('.final-sms');
-
-const countdownDate = new Date('01 01, 2023 00:00:00').getTime();
-
-let interval = setInterval(function(){
-//Obtener fecha actual y milisegundos
-    const now = new Date().getTime();
-
-    //Obtener las distancias entre ambas fechas
-    let distance = countdownDate - now;
-
-    //Calculos a dias-horas-minutos-segundos
-    let days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    let hours = Math.floor((distance % (1000 * 60 * 60 * 24 )) / (1000 * 60 * 60));
-    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    let seconds = Math.floor((distance % (1000 * 60 )) / (1000));
-
-    //Escribimos resultados
-    $days.innerHTML = days;
-    $hours.innerHTML = hours;
-    $minutes.innerHTML = minutes;
-    $seconds.innerHTML = ('0' + seconds).slice(-2);
-
-    //Cuando llegue a 0
-    if(distance < 0){
-        clearInterval(interval);
-        $finalMessage.style.transform = 'translateY(0)';
-    }
-}, 1000); 
-
-
-/* const $daysNode = document.getElementById('days'),
+const $daysNode = document.getElementById('days'),
 $hoursNode = document.getElementById('hours'),
 $minutesNode = document.getElementById('minutes'),
 $secondsNode = document.getElementById('seconds'),
@@ -67,5 +32,5 @@ const timeCountDown = () => {
 const formatTime = (time) => time > 10 ? time : `${time}`;
 
 timeCountDown();
-setInterval(timeCountDown, 1000); */
+setInterval(timeCountDown, 1000);
 
