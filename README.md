0513c9df2467dc239ddbd20278e411a202e3a7ee
Rainier Ríos <96790657+Nedbor@users.noreply.github.com>
2022-12-25 15:09:08 -0500

    Actualizacion estetica y de codigo

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # NewYearCountdown
 ## Web page made with HTML and CSS
+
+# Xmas Countdown
+Web page New year countdown
+
+## Technologies
+* HTML
+* CSS
+* JavaScript
+
+## Deploy 
+https://nyearcd.netlify.app
--- code.js
+++ code.js
@@ -1,47 +1,72 @@
-const getRemainTime = deadline => {
-    let now = new Date(),
-        remainTime = (new Date(deadline) - now + 1000) / 1000,
-        remainSeconds = ('0' + Math.floor(remainTime % 60)).slice(-2),
-        remainMinutes = ('0' + Math.floor(remainTime / 60 % 60)).slice(-2),
-        remainHours = ('0' + Math.floor(remainTime / 3600 % 24)).slice(-2),
-        remainDays = (Math.floor(remainTime / (3600 * 24)));
+const $days = document.getElementById('days'),
+$hours = document.getElementById('hours'),
+$minutes = document.getElementById('minutes'),
+$seconds = document.getElementById('seconds'),
+$finalMessage = document.querySelector('.final-sms');
 
-        
+const countdownDate = new Date('01 01, 2023 00:00:00').getTime();
 
-        return{
-            remainTime,
-            remainSeconds,
-            remainMinutes,
-            remainHours,
-            remainDays
-        }
+let interval = setInterval(function(){
+//Obtener fecha actual y milisegundos
+    const now = new Date().getTime();
+
+    //Obtener las distancias entre ambas fechas
+    let distance = countdownDate - now;
+
+    //Calculos a dias-horas-minutos-segundos
+    let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((distance % (1000 * 60 * 60 * 24 )) / (1000 * 60 * 60));
+    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((distance % (1000 * 60 )) / (1000));
+
+    //Escribimos resultados
+    $days.innerHTML = days;
+    $hours.innerHTML = hours;
+    $minutes.innerHTML = minutes;
+    $seconds.innerHTML = ('0' + seconds).slice(-2);
+
+    //Cuando llegue a 0
+    if(distance < 0){
+        clearInterval(interval);
+        $finalMessage.style.transform = 'translateY(0)';
+    }
+}, 1000); 
+
+/* const daysNode = document.getElementById('days');
+const hoursNode = document.getElementById('hours');
+const minutesNode = document.getElementById('minutes');
+const secondsNode = document.getElementById('seconds');
+
+const currentYear = new Date().getFullYear();
+const newyear = new Date(`December 25 ${currentYear}`);
+
+const timeCountDown = () => {
+    const currentDate = new Date();
+    const newYearDate = new Date(newyear);
+    const totalSeconds = (newYearDate - currentDate) / 1000;
+
+    const days = Math.floor(totalSeconds / 3600 / 24 );
+    const hours = Math.floor(totalSeconds / 3600)  % 24;
+    const minutes = Math.floor(totalSeconds / 60) %60;
+    const seconds = Math.floor(totalSeconds) % 60;
+
+    daysNode.innerHTML = formatTime(days);
+    hoursNode.innerHTML = formatTime(hours);
+    minutesNode.innerHTML = formatTime(minutes);
+    secondsNode.innerHTML = formatTime(seconds);
 };
 
-const countdown = (deadline, countdown, finalMessage) => {
-    const el = document.getElementById(countdown);
+const formatTime = (time) => time > 10 ? time : `0${time}`;
 
-    const timerUpdate = setInterval( () =>  {
-        let d = getRemainTime(deadline);
-        
-        el.innerHTML = `${d.remainDays}:${d.remainHours}:${d.remainMinutes}:${d.remainSeconds}`;
-
-        if (d.remainTime <= 1) {
-            clearInterval(timerUpdate);
-            el.innerHTML = finalMessage;
-
-        }
-    },1000)
-}
-
-countdown('Jan 01 2023 00:00:00 GMT-5', 'countdown', 'Happy New Year!');
-
+timeCountDown();
+setInterval(timeCountDown, 1000); */
 
 // Some Snow Falling by Codepen.io
 
 'use strict';
 
 const LIFE_PER_TICK = 1000 / 60;
-const MAX_FLAKES = Math.min(70, screen.width / 1280 * 75);
+const MAX_FLAKES = Math.min(75, screen.width / 1280 * 75);
 const flakes = [];
 const period = [
     n => 5 * (Math.sin(n)),
@@ -73,7 +98,7 @@ const cssString = `.snowfall-container {
 
   .snowflake {
     pointer-events: none;
-    color: #78D9FD;
+    color: #ddf;
     display: block;
     font-size: 2vw;
     left: -1vw;
